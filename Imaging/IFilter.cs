b9405b3f9a5a64dd8bd12083583d7fd884ba9731
lilmudd <johncgilliland@gmail.com>
2009-03-24 12:55:45 -0400

    Initial commit.

---
+++ Imaging/IFilter.cs
@@ -0,0 +1,35 @@
+﻿/*
+ *  DATE:  24 MARCH 2009
+ *  AUTHOR:  John C. Gilliand
+ *  FILENAME: IFilter.cs
+ *  
+ *  Interface for image processing filters.
+ */
+
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace Imaging
+{
+    /// <summary>
+    /// Interface for image processing filters. 
+    /// </summary>
+    public class IFilter
+    {
+        /// <summary>
+        /// Apply filter to image data.
+        /// </summary>
+        /// <param name="imageData">Source image data to apply filter to.</param>
+        /// <returns>Resulting bitmap image with filter applied.</returns>
+        public Bitmap Apply(BitmapData imageData);
+
+        /// <summary>
+        /// Apply filter to image.
+        /// </summary>
+        /// <param name="image">Source image to apply filter to.</param>
+        /// <returns>Resulting bitmap image with filter applied.</returns>
+        public Bitmap Apply(Bitmap image);
+    }
+}
